9d3ea984ef2116aaf8e9bc9f55b6078699638c2f
+ <+>
2023-06-21 00:39:17 +0300

    Updated the TaskList with the code for fetching tasks

---
+++ src/components/TaskList.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const response = await fetch('http://localhost:3000/tasks');
+    const data = await response.json();
+    setTasks(data);
+  };
+
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:3000/tasks/${id}`, {
+      method: 'DELETE',
+    });
+    fetchTasks();
+  };
+
+  return (
+    <div>
+      <h2>Task List</h2>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          <button onClick={() => handleDelete(task.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
